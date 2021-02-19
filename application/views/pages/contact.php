9c8e8dc175f9f3e73becdf8dd758373076a30d78
pj-commits <78728007+pj-commits@users.noreply.github.com>
2021-02-19 23:15:22 +0800

    Update contact.php

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -1,1 +1,38 @@
-
+//contact.php
+<section>
+	<div class="top-contact">
+		<div class="top-about-text">
+			<h1>Contact Us</h1>
+			<hr>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo
+			d tempor incididunt ut labore et dolore magna aliqua.</p>
+		</div>
+	</div>
+	<div class="forms">
+		<form>
+			
+			<div class="form-group">
+				<div class="row">
+					<div class="col">
+						<label for="exampleFormControlInput1">First Name</label>
+						<input type="text" class="formcontrol" placeholder="Enter FirsName">
+					</div>
+					
+					<div class="col">
+						<label for="exampleFormControlInput1">Last Name</label>
+						<input type="text" class="formcontrol" placeholder="Enter LastName">
+					</div>
+					
+				</div>
+					<label for="exampleFormControlInput1">Email address</label>
+					<input type="email" class="formcontrol" id="exampleFormControlInput1" placeholder="name@example.com">
+			</div>
+			
+			<div class="form-group">
+				<label for="exampleFormControlTextarea1">Example textarea</label>
+				<textarea class="formcontrol" id="exampleFormControlTextarea1" rows="3"></textarea>
+			</div>
+			<button type="submit" class="btn btn-success btn-sm pullright">Submit</button>
+		</form>
+	</div>
+</section>
