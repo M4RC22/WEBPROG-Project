c065ae7f00fdfd54ba9e82e5a4c4ccc62dac9961
lmbinotapa <78718421+lmbinotapa@users.noreply.github.com>
2021-02-19 20:16:23 +0800

    Create footer.php
    Footer

---
+++ application/views/templates/footer.php
@@ -0,0 +1,31 @@
+<footer>
+ <div class="content">
+ <div class="footer-logo">
+ <img src="<?php echo base_url('assets/images/logo.png'); ?>" alt="log
+o" width="40px" height="30px"/>
+ <h1>ALCOFIL</h1>
+ </div>
+ <div class="footer-contacs">
+ <p>+63906-209-7362</p>
+ <p>+63908-682-3594</p>
+ <p>+63961-122-3839</p>
+ </div>
+ </div>
+ <div class="icons">
+ </div>
+ <p class="reserved">&copy; 2021 Brewing Minds | All rights reserved.</p>
+</footer>
+<script src="<?php echo base_url("/assets/js/nav.js"); ?>"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-
+KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="an
+onymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.m
+in.js" integrity="sha384-
+ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="an
+onymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+integrity="sha384-
+JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="an
+onymous"></script>
+</body>
+</html>
