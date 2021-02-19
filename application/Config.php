36858c58e0bc4e70d4d5b1a991fb32cb5d2a30eb
lmbinotapa <78718421+lmbinotapa@users.noreply.github.com>
2021-02-19 20:11:49 +0800

    Create Config.php
    Url Configuration

---
+++ application/Config.php
@@ -0,0 +1,11 @@
+Set $config['base_url']
+
+To $config['base_url'] = 'http://localhost/Alcofil';
+
+Set $config['index_page']
+
+To $config['index_page'] = 'index.php';
+
+Set $config['uri_protocol']
+
+To $config['uri_protocol'] = 'REQUEST_
