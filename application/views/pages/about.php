0b61800008a12402a5b5a591bf72fe01f9ff9281
Manadero-Michelle <70443919+Manadero-Michelle@users.noreply.github.com>
2021-02-19 23:12:42 +0800

    About Page
    About-Us section (committed by Michelle Manadero)

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -7,3 +7,56 @@
 d tempor incididunt ut labore et dolore magna aliqua.</p>
  </div>
  </div>
+
+	<div class="about-us-container">
+ <div class="about-us">
+ <div class="about-us-left">
+ <h1>Who we are</h1>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d
+o eiusmod tempor incididunt ut labore et dolore magna aliqua.
+ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do e
+iusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+ </div>
+ <!-- CSS for this div is on home.css -->
+ <div class="about-us-right">
+ <div id="carouselExampleIndicators" class="carousel slide" dataride="carousel" style="z-index:1;">
+ <ol class="carousel-indicators">
+ <li data-target="#carouselExampleIndicators" data-slideto="0" class="active"></li>
+ <li data-target="#carouselExampleIndicators" data-slideto="1"></li>
+ <li data-target="#carouselExampleIndicators" data-slideto="2"></li>
+ <li data-target="#carouselExampleIndicators" data-slideto="3"></li>
+ <li data-target="#carouselExampleIndicators" data-slideto="4"></li>
+ <li data-target="#carouselExampleIndicators" data-slideto="5"></li>
+ </ol>
+ <div class="carousel-inner">
+ <div class="carousel-item active">
+ <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic1.jpg'); ?>" alt="First sli
+de">
+ </div>
+ <div class="carousel-item">
+ <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic2.jpg'); ?>" alt="Second sl
+ide">
+ </div>
+<div class="carousel-item">
+ <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic3.jpg'); ?>" alt="Third sli
+de">
+ </div>
+<div class="carousel-item">
+	
+	<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic4.jpg'); ?>" alt="Second sl
+ide">
+ </div>
+<div class="carousel-item">
+ <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic5.jpg'); ?>" alt="Third sli
+de">
+ </div>
+<div class="carousel-item">
+ <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic6.jpg'); ?>" alt="Second sl
+ide">
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+ </section> //End of First Section
