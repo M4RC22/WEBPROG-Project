769772e7b711326a4a16d6d5b63073f6bb619be9
Manadero-Michelle <70443919+Manadero-Michelle@users.noreply.github.com>
2021-02-19 23:47:48 +0800

    Edited comments
    Just changed and added some code comments - Michelle Manadero

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,13 +1,13 @@
-<section> //First Section
+<section> <!--First Section-->
  <div class="top-about">
  <div class="top-about-text">
  <h1>About Alcofil</h1>
  <hr>
  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
  </div>
- </div> // End of top-about class div
+ </div> <!--End of top-about class div-->
 
-<div class="about-us-container"> //about-us-container div class start
+<div class="about-us-container"> <!--about-us-container div class start-->
  	<div class="about-us">
  	<div class="about-us-left">
  
@@ -28,7 +28,7 @@ consectetur adipiscing elit, sed do e iusmod tempor incididunt ut labore et dolo
 		 <li data-target="#carouselExampleIndicators" data-slideto="5"></li>
 	 </ol>
 	 
-//From here are the carousel-styled images	 
+<!--From here are the carousel-styled images--> 
  <div class="carousel-inner">
  	<div class="carousel-item active">
  		<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic1.jpg'); ?>" alt="First slide">
@@ -53,31 +53,31 @@ consectetur adipiscing elit, sed do e iusmod tempor incididunt ut labore et dolo
 	 <div class="carousel-item">
  		<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic6.jpg'); ?>" alt="Second slide">
  	</div>
- </div> // Carousel style div end
- </div> // carouselExampleIndicators ID end
- </div> // div class="about-us-right" end
- </div> // div class="about-us" end
- </div> // div class="about-us-container" end
- </section> //End of First Section
+ </div> <!-- Carousel style div end -->
+ </div> <!-- carouselExampleIndicators ID end -->
+ </div> <!-- div class="about-us-right" end -->
+ </div> <!-- div class="about-us" end -- >
+ </div> <!-- div class="about-us-container" end-->
+ </section> <!--End of First Section-->
 
-//Start of second section
+<!--Start of Second Section-->
 <section>
  <div class="mission-vision">
- 	<div class="mission"> //Mission div
+ 	<div class="mission"> <!--Mission div-->
  		<img src="<?php echo base_url('assets/images/rocket.jpg'); ?>"alt =""/>
  
 	 	<h1>Our Mission</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 	 </div>
 	 
- 	<div class="vision"> //Vision div
+ 	<div class="vision"> <!--Vision div-->
  		<img src="<?php echo base_url('assets/images/glass.jpg'); ?>"alt =""/>
  		
 		<h1>Our Vission</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
  		</div>
 	 
- </div> //End of Mission Vision div
+ </div> <!--End of Mission Vision div-->
 </section>
 
 
