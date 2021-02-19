4fb78db8b85a2dc007e4e33e4b7a0ff04559a7d0
Manadero-Michelle <70443919+Manadero-Michelle@users.noreply.github.com>
2021-02-19 23:19:41 +0800

    Updated about.php
    Added comments on div start and ends

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,62 +1,61 @@
-<section>
+<section> //First Section
  <div class="top-about">
  <div class="top-about-text">
  <h1>About Alcofil</h1>
  <hr>
- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo
-d tempor incididunt ut labore et dolore magna aliqua.</p>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
  </div>
- </div>
+ </div> // End of top-about class div
 
-	<div class="about-us-container">
- <div class="about-us">
- <div class="about-us-left">
- <h1>Who we are</h1>
- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed d
-o eiusmod tempor incididunt ut labore et dolore magna aliqua.
- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do e
-iusmod tempor incididunt ut labore et dolore magna aliqua.</p>
- </div>
+<div class="about-us-container"> //about-us-container div class start
+ 	<div class="about-us">
+ 	<div class="about-us-left">
+ 
+		<h1>Who we are</h1>
+ 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit, sed do e iusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+ 	</div>
+	 
  <!-- CSS for this div is on home.css -->
  <div class="about-us-right">
  <div id="carouselExampleIndicators" class="carousel slide" dataride="carousel" style="z-index:1;">
- <ol class="carousel-indicators">
- <li data-target="#carouselExampleIndicators" data-slideto="0" class="active"></li>
- <li data-target="#carouselExampleIndicators" data-slideto="1"></li>
- <li data-target="#carouselExampleIndicators" data-slideto="2"></li>
- <li data-target="#carouselExampleIndicators" data-slideto="3"></li>
- <li data-target="#carouselExampleIndicators" data-slideto="4"></li>
- <li data-target="#carouselExampleIndicators" data-slideto="5"></li>
- </ol>
+	 <ol class="carousel-indicators">
+		 <li data-target="#carouselExampleIndicators" data-slideto="0" class="active"></li>
+		 <li data-target="#carouselExampleIndicators" data-slideto="1"></li>
+		 <li data-target="#carouselExampleIndicators" data-slideto="2"></li>
+		 <li data-target="#carouselExampleIndicators" data-slideto="3"></li>
+		 <li data-target="#carouselExampleIndicators" data-slideto="4"></li>
+		 <li data-target="#carouselExampleIndicators" data-slideto="5"></li>
+	 </ol>
+	 
+//From here are the carousel-styled images	 
  <div class="carousel-inner">
- <div class="carousel-item active">
- <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic1.jpg'); ?>" alt="First sli
-de">
- </div>
- <div class="carousel-item">
- <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic2.jpg'); ?>" alt="Second sl
-ide">
- </div>
-<div class="carousel-item">
- <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic3.jpg'); ?>" alt="Third sli
-de">
- </div>
-<div class="carousel-item">
-	
-	<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic4.jpg'); ?>" alt="Second sl
-ide">
- </div>
-<div class="carousel-item">
- <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic5.jpg'); ?>" alt="Third sli
-de">
- </div>
-<div class="carousel-item">
- <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic6.jpg'); ?>" alt="Second sl
-ide">
- </div>
- </div>
- </div>
- </div>
- </div>
- </div>
+ 	<div class="carousel-item active">
+ 		<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic1.jpg'); ?>" alt="First slide">
+ 	</div>
+	 
+ 	<div class="carousel-item">
+ 		<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic2.jpg'); ?>" alt="Second slide">
+ 	</div>
+
+	<div class="carousel-item">
+ 		<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic3.jpg'); ?>" alt="Third slide">
+ 	</div>
+
+	 <div class="carousel-item">
+		<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic4.jpg'); ?>" alt="Second slide">
+ 	</div>
+	 
+	<div class="carousel-item">
+		 <img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic5.jpg'); ?>" alt="Third slide">
+	 </div>
+
+	 <div class="carousel-item">
+ 		<img class="d-block w-75 mxauto" src="<?php echo base_url('assets/images/grid_pic6.jpg'); ?>" alt="Second slide">
+ 	</div>
+ </div> // Carousel style div end
+ </div> // carouselExampleIndicators ID end
+ </div> // div class="about-us-right" end
+ </div> // div class="about-us" end
+ </div> // div class="about-us-container" end
  </section> //End of First Section
