db18c1a7596e6c8f70ce0fa9d446c460e11425c1
Manadero-Michelle <70443919+Manadero-Michelle@users.noreply.github.com>
2021-02-19 23:09:26 +0800

    Create about.php
    This is the About Page (committed by Michelle Manadero)

---
+++ application/views/pages/about.php
@@ -0,0 +1,9 @@
+<section>
+ <div class="top-about">
+ <div class="top-about-text">
+ <h1>About Alcofil</h1>
+ <hr>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo
+d tempor incididunt ut labore et dolore magna aliqua.</p>
+ </div>
+ </div>
