00b663d6cbfcc4b6c90d71b5762700fbfcbf8db3
Marc Ricahuerta <78727221+M4RC22@users.noreply.github.com>
2021-02-20 12:22:47 +0800

    php file for home page

---
+++ application/views/pages/home.php
@@ -0,0 +1,156 @@
+<section>
+    <div class="container-home">
+        <div class="home">
+            <div class="left-col">
+                <h1>Cheapest Alcohol Refilling Station in the Country</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            <div class="right-col">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="z-index:1;">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                        <img class="d-block w-75 mx-auto" src="<?php echo base_url('assets/images/prod_pump.png'); ?>" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-75 mx-auto" src="<?php echo base_url('assets/images/prod_sprayBottle.png'); ?>" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                        <img class="d-block w-75 mx-auto" src="<?php echo base_url('assets/images/prod_sprayBottle1.png'); ?>" alt="Third slide">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wave">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 170"><path fill="#fff" fill-opacity="1" d="M0,32L48,37.3C96,43,192,53,288,53.3C384,53,480,43,576,69.3C672,96,768,160,864,165.3C960,171,1056,117,1152,96C1248,75,1344,85,1392,90.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </div>
+    <div class="text-gallery">
+            <p>Ang alcohol ng bayan</p>
+        </div>
+ <!-- Info Section -->       
+<section>
+
+    <div class="info-container">
+        <div class="info-img">
+            <img src="<?php echo base_url('assets/images/prod_wrap.png'); ?>" alt="bottle" data-aos="slide-right">
+        </div>
+        <div class="info-text" data-aos="slide-left">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+    </div>
+    <h1 class="more-info-title">Why Alcofil?</h1>
+    <div class="more-info">
+        <div class="col1" data-aos="slide-right">
+            <h1>01</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        <div class="col2" data-aos="slide-up" data-aos-delay="200">
+            <h1>02</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        <div class="col3" data-aos="slide-left" data-aos-delay="250">
+            <h1>03</h1>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    </div>
+</section>
+<!-- Ads Section -->
+</section>
+    <div class="banner">
+        <h1 class="banner-heading">Save Lives for as low as<span> &#8369;</span>1</h1>
+        <p class="line"><span>&#128505; </span>Easy Access <span>&#128505; </span>Eco-friendly <span>&#128505; </span>Budget Friendly</p>
+    </div>>
+</section>
+<!-- Info Banner -->
+<section>
+    <div class="info-banner">
+        <div class="info-row1">
+            <div class="row1-left" data-aos="slide-up">
+                <img src="<?php echo base_url('assets/images/easyAccess.png'); ?>" alt="" />
+            </div>
+            <div class="row1-right" data-aos="slide-left">
+                <h1>Easy Acess</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+        <div class="info-row2">
+            <div class="row2-left" data-aos="slide-right">
+                <h1>Eco-Friendly</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <div class="row2-right" data-aos="slide-up">
+                <img src="<?php echo base_url('assets/images/ecoFriendly.png'); ?>" alt=""/>
+            </div>
+        </div>
+        <div class="info-row3" data-aos="slide-up">
+            <div class="row3-left">
+                <img src="<?php echo base_url('assets/images/budgetFriendly.png'); ?>" alt=""/>
+            </div>
+            <div class="row3-right" data-aos="slide-left">
+                <h1>Budget Friendly</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Hiring Section -->
+<section>
+    <div class="hiring-ads-container">
+        <div class="hiring-ads">
+            <div class="ads-text">
+                    <h1>We are looking for employees!</h1>
+                    <h6>No experience Nedeed</h6>
+                    <a class="hiring-toggle" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Be Part of the Team!</a>
+                </div>
+                <div class="ads-img">
+                    <img src="<?php echo base_url('assets/images/hiring.png'); ?>" alt="Hiring banner pic" height="300px"/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="collapse" id="collapseExample">
+         <div class="accordion">
+            <div class="col"></div>
+                <button type="btn" class="close" aria-label="Close" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <div class="card card-body" id="accordion">
+                    <p>Download the file > Fill-up the form > Attach file > Submit</p>
+                    <form>
+                        <div class="form-group">
+                    <form>
+                        <div class="row">
+                            <div class="col">
+                            <label for="exampleFormControlInput1">First Name</label>
+                            <input type="text" class="form-control" placeholder="Enter FirsName">
+                            </div>
+                            <div class="col">
+                            <label for="exampleFormControlInput1">Last Name</label>
+                            <input type="text" class="form-control" placeholder="Enter LastName">
+                            </div>
+                        </div>
+                    </form>
+                            <label for="exampleFormControlInput1">Email address</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Email">
+                            <label for="exampleFormControlInput1">Contact Number</label>
+                            <input type="tel" class="form-control" id="exampleFormControlInput1" placeholder="Enter Contact Number">
+                            <button type="submit" class="btn btn-primary btn-sm  mt-3">Download File</button>
+                            <button type="submit" class="btn btn-success btn-sm mt-3" class="float-center">Attach File</button>
+                            <button type="submit" class="btn pull-right mt-3">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="curved">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,160L1440,32L1440,320L0,320Z"></path></svg>     
+    </div>
+</section>
+
