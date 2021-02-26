11f638cbb9971608e142f6286736839961382909
pj-commits <78728007+pj-commits@users.noreply.github.com>
2021-02-26 21:25:27 +0800

    Update contact.php

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -1,40 +1,39 @@
 <!--contact.php-->
 <section>
-
-	<div class="top-contact">
-		<div class="top-about-text">
-			<h1>Contact Us</h1>
-			<hr>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmo
-			d tempor incididunt ut labore et dolore magna aliqua.</p>
-		</div>
-	</div>
-
-	<div class="forms">
-		<form>
-			<div class="form-group">
-				<div class="row">
-					<div class="col">
-						<label for="exampleFormControlInput1">First Name</label>
-						<input type="text" class="formcontrol" placeholder="Enter FirsName">
-					</div>
-
-					<div class="col">
-						<label for="exampleFormControlInput1">Last Name</label>
-						<input type="text" class="formcontrol" placeholder="Enter LastName">
-					</div>
-
-				</div>
-					<label for="exampleFormControlInput1">Email address</label>
-					<input type="email" class="formcontrol" id="exampleFormControlInput1" placeholder="name@example.com">
-			</div>
-
-			<div class="form-group">
-				<label for="exampleFormControlTextarea1">Example textarea</label>
-				<textarea class="formcontrol" id="exampleFormControlTextarea1" rows="3"></textarea>
-			</div>
+<div class="top-contact">
+        <div class="top-about-text">
+        <h1>Contact Us</h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    </div> 
+	
+    <div class="forms">
+        <form method="post" action="<?php echo base_url('feedback/send'); ?>" enctype="multipart/form-data">
+            <div class="form-group">
+                <div class="row">
+                    <div class="col">
+                        <label for="exampleFormControlInput1">First Name</label>
+                        <input type="text" name="fname" class="form-control" placeholder="Enter FirsName"  class="form-control" required>
+                    </div>
 			
-			<button type="submit" class="btn btn-success btn-sm pullright">Submit</button>
-		</form>
-	</div>
+                    <div class="col">
+                        <label for="exampleFormControlInput1">Last Name</label>
+                        <input type="text" name="lname" class="form-control" placeholder="Enter LastName"  class="form-control" required>
+                    </div>
+                </div>
+		    
+                <label for="exampleFormControlInput1">Email address</label>
+                <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Email"  class="form-control" required>
+            </div>
+		
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                <textarea name="text-area" class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
+            </div>
+		
+            <button type="submit" name ="submit" value="Submit" class="btn pull-right mt-3">Submit</button>
+        </form>
+    </div>
+
 </section>
