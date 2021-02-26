b286b32493f14bed71800afa5e3268c4e748bd8e
Manadero-Michelle <70443919+Manadero-Michelle@users.noreply.github.com>
2021-02-26 10:31:06 +0800

    Updated email.php
    Added big image section on email.php file

--- application/views/templates/email.php
+++ application/views/templates/email.php
@@ -33,6 +33,67 @@
 				    
 				     <tr>
                                     <td align="center">
+					    
+<!-- big image section -->
+    <table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="ffffff" class="bg_color">
+
+        <tr>
+            <td align="center">
+                <table border="0" align="center" width="590" cellpadding="0" cellspacing="0" class="container590">
+                    <tr>
+
+                        <td align="center" class="section-img">
+                            <a href="" style=" border-style: none !important; display: block; border: 0 !important;">
+                            <img src="<?php echo base_url('assets/images/grid_pic1.jpg'); ?>" style="display: block; width: 590px;" width="590" border="0" alt="picture" /></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td height="20" style="font-size: 20px; line-height: 20px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td align="center" style="color: #343434; font-size: 24px; font-family: Quicksand, Calibri, sans-serif; font-weight:700;letter-spacing: 3px; line-height: 35px;"
+                            class="main-header">
+
+
+                            <div style="line-height: 35px"> Thank You <span style="color: #b9191c;">'.$this->input->post("lname").'</span>
+
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td height="10" style="font-size: 10px; line-height: 10px;">&nbsp;</td>
+                    </tr>
+
+                    <tr>
+                        <td align="center">
+                            <table border="0" width="40" align="center" cellpadding="0" cellspacing="0" bgcolor="eeeeee">
+                                <tr>
+                                    <td height="2" style="font-size: 2px; line-height: 2px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td height="20" style="font-size: 20px; line-height: 20px;">&nbsp;</td>
+                    </tr>
+
+                    <tr>
+                        <td align="center">
+                            <table border="0" width="400" align="center" cellpadding="0" cellspacing="0" class="container590">
+                                <tr>
+                                    <td align="center" style="color: #888888; font-size: 16px; font-family: 'Work Sans', Calibri, sans-serif; line-height: 24px;">
+
+
+                                        <div style="line-height: 24px">
+
+                                            Thank you so much for the feedback you provided. We value your thoughts and we are willing to strive harder for able to give our best. 
+
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
                                         <table width="360 " border="0" cellpadding="0" cellspacing="0" style="border-collapse:collapse;"
                                             class="container590 hide">
                                             <tr>
