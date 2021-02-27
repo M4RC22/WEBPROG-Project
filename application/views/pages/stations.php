06cd3a9453a7dc07c9be883afe833113a578b504
Marc Ricahuerta <78727221+M4RC22@users.noreply.github.com>
2021-02-27 16:35:57 +0800

    Create stations.php

---
+++ application/views/pages/stations.php
@@ -0,0 +1,47 @@
+<section>
+    <div class="top-stations">
+        <div class="top-stations-text">
+            <h1>Stations of Alcofil</h1>
+            <hr>
+            <p id="map-click">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+    </div>
+        <div class="map-container" > 
+            <div id="mapid"></div>
+        </div>
+        <div class="stations-container">
+        <h1 class="stations-text">Stations</h1>
+            <div class="stations">
+                <div class="stations-col1">
+                        <h1>Asia Pacific College</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                        <a href="#map-click" onclick="goToStation('s1')">Go Here</a>
+                </div>
+                <div class="stations-col2">
+                        <h1>Makati Hope Christian School</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                        <a href="#map-click" onclick="goToStation('s2')">Go Here</a>
+                </div>
+                <div class="stations-col3">
+                        <h1>Pasay City South High School</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                        <a href="#map-click" onclick="goToStation('s3')">Go Here</a>
+                </div>
+                <div class="stations-col4">
+                        <h1>Polytechnic University of the Philippines</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                        <a href="#map-click" onclick="goToStation('s4')">Go Here</a>
+                </div>
+                <div class="stations-col5">
+                        <h1>Paranaque City Hall Complex</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                        <a href="#map-click" onclick="goToStation('s5')">Go Here</a>
+                </div>
+                <div class="stations-col6">
+                        <h1>UP Diliman</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                        <a href="#map-click" onclick="goToStation('s6')">Go Here</a>
+                </div>
+            </div>
+        </div>
+</section>
